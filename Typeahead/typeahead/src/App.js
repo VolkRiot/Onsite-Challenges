957830dd8c5330341448bec11034e801e2c387d5
Mikhail Metrikin <metrikin@gmail.com>
2019-06-25 16:26:51 -0700

    Add challenge

---
+++ Typeahead/typeahead/src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputValue: '',
+      activeSuggestions: []
+    };
+
+    this.autoCompleteOptions = [
+      'bat',
+      'batman',
+      'baterang',
+      'beans',
+      'bother',
+      'cat',
+      'catorce',
+      'captain america',
+      'capital letter',
+    ];
+
+    this.debounceTimeout = null;
+  }
+
+  handleChange = e => {
+    clearTimeout(this.debounceTimeout);
+    this.debounceTimeout = setTimeout(() => {
+      this.state.inputValue && this.runAutoComplete();
+    }, 800);
+    this.setState({ inputValue: e.target.value, activeSuggestions: [] });
+  };
+
+  runAutoComplete = () => {
+    const newSuggested = [];
+    for (let phrase of this.autoCompleteOptions) {
+      if (phrase.includes(this.state.inputValue)) {
+        newSuggested.push(phrase);
+      }
+    }
+
+    this.setState({ activeSuggestions: newSuggested });
+  };
+
+  render() {
+    const { inputValue } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <input
+            style={{ width: 300, marginBottom: 2, boxSizing: 'border-box' }}
+            value={inputValue}
+            onChange={this.handleChange}
+          />
+          {this.state.activeSuggestions.length ? (
+            <div
+              style={{ height: 100, width: 300, backgroundColor: 'white' }}
+            >
+              {this.state.activeSuggestions.map((phrase, index) => (
+                <div key={index}>
+                  <span style={{color: 'black'}}>{phrase}</span>
+                  <br/>
+                </div>
+              ))}
+            </div>
+          ) : null}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
